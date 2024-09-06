0682cfafb867729c6f6cb4efe16e0c6476b2ae26
Ivan Garrahan <psyig2@exmail.nottingham.ac.uk>
2024-09-06 10:55:58 +0100

    feat: added second val test for invalid username

--- src/test/java/com/ivan/videostreamingapp/CreateUserValidationTests.java
+++ src/test/java/com/ivan/videostreamingapp/CreateUserValidationTests.java
@@ -1,6 +1,7 @@
 package com.ivan.videostreamingapp;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -9,9 +10,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-
-import java.util.Calendar;
-import java.util.Date;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -37,10 +35,17 @@ public class CreateUserValidationTests {
 //        validUser = new User("johnDoe", "Password123", "john@example.com", "1990-01-01", "1234567812345678");
         validUser = new User("johnDoe", "Password123", "john@example.com",
                 "1999-01-21", "1234567812345678");
+
+    }
+
+    @AfterEach
+    public void resetUser() {
+        validUser = new User("johnDoe", "Password123", "john@example.com",
+                "1999-01-21", "1234567812345678");
     }
 
     @Test
-    public void whenValidUser_thenReturns201() throws Exception {
+    public void testEnterValidUser() throws Exception {
         Mockito.when(userService.createUser(Mockito.any(User.class))).thenReturn(validUser);
 
         mvc.perform(post("/api/users")
@@ -48,5 +53,19 @@ public class CreateUserValidationTests {
                         .content(objectMapper.writeValueAsString(validUser)))
                 .andExpect(status().isCreated());
     }
+
+    @Test
+    public void testEnterInvalidUsername() throws Exception {
+        validUser.setUsername(null);
+
+        Mockito.when(userService.createUser(Mockito.any(User.class))).thenReturn(validUser);
+
+        mvc.perform(post("/api/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(validUser)))
+                .andExpect(status().isBadRequest());
+
+    }
+
 
 }
